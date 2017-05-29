3a792afc8a95935fba9bcf866de31541d11b04ab
DaNkOMeN <dankogol225@gmail.com>
2017-05-30 01:05:43 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,164 +1,239 @@
 #include <stdio.h>
 #include "graphics.h"
 #include <iostream>
+#include <locale.h>
 
 using namespace std;
 
 int const mash = 30;
-void setQueen(int i,int j,int vert,int gor, int **board,int QN) //поставить ферзя
+void setQueen(int i, int j, int vert, int gor, int **board)
 {
-    int minim=min(vert,gor);
-
-   for (int x = 0; x< minim;x++)
+    for (int counter = 0; counter < gor; counter++)
     {
-        board[x][j]++;  //идем вправо относительно клетки
-        board[i][x]++;  //идем вниз относительно клетки
-        int foo;
-        foo = j-i+x;
-        if (foo >=0 && foo<gor)//диагональ вправо вниз
-            board[x][foo]++;
-        foo = j+ i - x;
-         if (foo >=0 && foo<gor)//диагонал вправо вверх
-            board[x][foo]++;
-
+        board[i][counter]++;
+    };
+    for (int counter = 0; counter < vert; counter++)
+    {
+        board[counter][j]++;
+    };
+    int curI = i ,curJ = j;
+    while ((curI >= 0) && (curJ >= 0))
+    {
+        board[curI][curJ]++;
+        curI--;
+        curJ--;
     }
-    board[i][j]=-1;
+    curI = i ;curJ = j;
+    while ((curI < vert) && (curJ < gor))
+    {
+        board[curI][curJ]++;
+        curI++;
+        curJ++;
+    }
+    curI = i ;curJ = j;
+    while ((curI < vert) && (curJ >= 0))
+    {
+        board[curI][curJ]++;
+        curI++;
+        curJ--;
+    }
+    curI = i ;curJ = j;
+    while ((curI >= 0) && (curJ < gor))
+    {
+        board[curI][curJ]++;
+        curI--;
+        curJ++;
+    }
+    board[i][j] = -1;
+
 }
 
-void resetQueen(int i,int j, int vert,int gor, int **board,int QN)
+
+void resetQueen(int i, int j, int vert, int gor, int **board)
 {
-    int minim=min(vert,gor);
-    for (int x = 0; x< minim;x++)
+    for (int counter = 0; counter < gor; counter++)
     {
-
-        board[x][j]--;
-        board[i][x]--;
-        int foo;
-        foo = j - i + x;
-        if (foo >=0 && foo<gor)
-            board[x][foo]--;
-        foo = j + i - x;
-         if (foo >=0 && foo<gor)
-            board[x][foo]--;
-
+        board[i][counter]--;
+    };
+    for (int counter = 0; counter < vert; counter++)
+    {
+        board[counter][j]--;
+    };
+    int curI = i ,curJ = j;
+    while ((curI >= 0) && (curJ >= 0))
+    {
+        board[curI][curJ]--;
+        curI--;
+        curJ--;
     }
-    board[i][j]=0;
+    curI = i ;curJ = j;
+    while ((curI < vert) && (curJ < gor))
+    {
+        board[curI][curJ]--;
+        curI++;
+        curJ++;
+    }
+    curI = i ;curJ = j;
+    while ((curI < vert) && (curJ >= 0))
+    {
+        board[curI][curJ]--;
+        curI++;
+        curJ--;
+    }
+    curI = i ;curJ = j;
+    while ((curI >= 0) && (curJ < gor))
+    {
+        board[curI][curJ]--;
+        curI--;
+        curJ++;
+    }
+    board[i][j] = 0;
 }
 
-bool tryQueen(int i,int vert,int gor, int **board,int k,int QN)//Пытаемся поставить ферзя на i-ый столбец
+bool tryQueen(int i, int vert, int gor, int **board, int QN,int counter)
 {
-    bool result = false;
-    int counter = 0;
-    for (int j=0;j < gor ;j++)    //идем по столбцам
+    bool result = false ;
+    if (counter > QN)
     {
-        if (counter < QN)
-        {
-          if ((board[i][j]==0)  )  //если клетка не занята,
-           {
-            setQueen(i,j,vert,gor,board,QN);
-            counter++;  // тогда ставим ферзя
-            if (counter ==QN)        // и если вертикаль последняя
-                result = true;       //возвращает то, что мы поставили все ферзи
-            else
-              {                         //если поставлены не все ферзы
-               if(!(result =tryQueen(i+1,vert,gor,board,k,QN-1))) // если не смогли поставить на следующий столбец
-                resetQueen(i,j,vert,gor,board,QN);
-                   //отменяем наш ход, обнуцляем клетку
-               }
+        return true;
+    }
+    else
+    {
+      for (int j = 0; j < gor; j++)
+      {
+          if (board[i][j] == 0)
+          {
+            setQueen(i,j,vert,gor,board);
+            bool nextDone = tryQueen(i + 1,vert,gor,board,QN,counter + 1);
+            if (nextDone != true)
+            {
+               resetQueen(i,j,vert,gor,board);
             }
-
-        }
-        if (result)   //если поставили всех ферзй
-            break;    //то останавливаем
+            else
+            {
+               return true;
+            }
+          }
+      }
     }
     return result;
 }
 
-void printpole(int **board,int vert,int gor)
+
+void printpole(int **board, int vert, int gor, int **board2)
 {
-   int counter = 1;
-   for (int i=0;i<vert;i++)
-   {
-    for (int j=0;j<gor;j++)
-       {
-          if (board[i][j]==-1)
-          {
-              setfillstyle(1,4);
-              bar(j*mash,i*mash,(j+1)*mash,(i+1)*mash);
-          }
-          else
-            { if (board[i][j]==-10)
-                 setfillstyle(1,9);
-                 else
-                 {
-                    if (counter%2==1)
-                   setfillstyle(1,15);
-                   else
-                   setfillstyle(1,0);
-                   bar(j*mash,i*mash,(j+1)*mash,(i+1)*mash);
-                 }
+    int counter = 1;
+    for (int i = 0; i < vert; i ++)
+    {
+        for (int j = 0; j < gor; j ++)
+        {
+            if (board[i][j] == -1)
+            {
+                setfillstyle(1, 4);
+                bar(j * mash, i * mash,(j + 1) * mash,(i + 1) * mash);
             }
-            counter++;
+            else
+            {
+                if (board2[i][j] == 1){
+                    setfillstyle(1, 6);
+                    bar(j * mash, i * mash,(j + 1) * mash,(i + 1) * mash);
+                }
+                else
+                {
+                    if (counter % 2 == 1)
+                        setfillstyle(1, 15);
+                    else
+                        setfillstyle(1, 0);
+                    bar(j * mash, i * mash,(j + 1) * mash,(i + 1) * mash);
+                }
+            }
+            counter ++;
        }
-       if (gor%2!=1)
+       if (gor % 2 != 1)
          counter++;
        }
 }
+
 int main ()
 {
     setlocale(LC_ALL, "Russian");
-    cout<<"Введите размеры поля: "<<endl;
-    cout<<"Введите количество клеток по вертикали:"<<endl;
+    cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂС‹ РїРѕР»СЏ: " << endl;
+    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РєР»РµС‚РѕРє РїРѕ РІРµСЂС‚РёРєР°Р»Рё:" << endl;
     int vert;
-    cin>>vert;
-    cout<<"Введите количество клеток по горизонтали:"<<endl;
+    cin >> vert;
+    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РєР»РµС‚РѕРє РїРѕ РіРѕСЂРёР·РѕРЅС‚Р°Р»Рё:" << endl;
     int gor;
-    cin>>gor;
-    initwindow(gor*mash,vert*mash);
+    cin >> gor;
     int QN;
-    cout<<"Введите количество ферзей, которые хотите поставить:"<<endl;
-    cin>> QN;
-    if ((QN <= gor) && (QN <= vert) &&(QN >= 0))
+    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ С„РµСЂР·РµР№, РєРѕС‚РѕСЂС‹Рµ С…РѕС‚РёС‚Рµ РїРѕСЃС‚Р°РІРёС‚СЊ:" << endl;
+    cin >> QN;
+    if ((QN <= gor) && (QN <= vert) && (QN >= 0))
     {
-    int **board = new int *[vert];
-		for (int i = 0; i < vert; i++)
-		{
+        int **board = new int *[vert];
+		for (int i = 0; i < vert; i ++)
 			board[i] = new int[gor];
-		}
-    int k= vert-1;
-    for (int i=0;i<vert;i++)
-       for (int j=0;j<gor;j++)
-        board[i][j]=0;
-    cout<<"Хотите чтобы клетки исчезли? Y/y-да , N/n - нет"<<endl;
-    char b;
-    cin>>b;
-    if ((b == 'y')|| (b == 'Y'))
-    {
-        cout <<"Введите количество исчезнувших клеток: "<<endl;
-        int q;
-        cin >> q;
-        int i1,j1;
-       for (int l=0;l<q;l++)
-       {
-           cout<<"Введите координаты клетки:"<<endl;
-           cin>>i1;
-           cin>>j1;
-           board[i1][j1] = -10;
-       }
-      tryQueen(0,vert,gor,board,k, QN);
-      printpole(board,vert,gor);
+        int k = vert - 1;
+        for (int i = 0; i < vert; i ++)
+            for (int j = 0; j < gor; j ++)
+                board[i][j] = 0;
+
+
+        int **board2 = new int *[vert];
+		for (int i = 0; i < vert; i ++)
+			board2[i] = new int[gor];
+        for (int i = 0; i < vert; i ++)
+            for (int j = 0; j < gor; j ++)
+                board2[i][j] = 0;
+
+
+        cout << "РҐРѕС‚РёС‚Рµ С‡С‚РѕР±С‹ РєР»РµС‚РєРё РёСЃС‡РµР·Р»Рё? Y/y-РґР° , N/n - РЅРµС‚" << endl;
+        char b;
+        cin >> b;
+        if ((b == 'y') || (b == 'Y'))
+        {
+            cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РёСЃС‡РµР·РЅСѓРІС€РёС… РєР»РµС‚РѕРє: " << endl;
+            int q;
+            cin >> q;
+            int i1,j1;
+            for (int l = 0; l < q; l ++)
+            {
+                cout << "Р’РІРµРґРёС‚Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹ РєР»РµС‚РєРё:" << endl;
+                cin >> i1;
+                cin >> j1;
+                board2[i1-1][j1-1] = 1;
+            }
+
+            tryQueen(0, vert, gor, board, QN,0);
+            for (int i = 0; i < vert; i ++)
+            {
+                for (int j = 0; j < gor; j ++)
+                    cout << board[i][j]<<' ';
+                cout << endl;
+            }
+            initwindow(gor * mash, vert * mash);
+            printpole(board, vert, gor,board2);
+        }
+        else
+            if  ((b == 'n')|| (b == 'N'))
+            {
+
+                tryQueen(0, vert, gor, board, QN ,1);
+                for (int i = 0; i < vert; i ++)
+                    {
+                        for (int j = 0; j < gor; j ++)
+                            cout << board[i][j]<<' ';
+                        cout << endl;
+                    }
+                initwindow(gor * mash, vert * mash);
+                printpole(board, vert, gor,board2);
+            }
+            else cout << "Р’РІРµРґРµРЅРѕ РЅРµРІРµСЂРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ!!!" << endl;
     }
-    else if  ((b == 'n')|| (b == 'N'))
-     {
-      tryQueen(0,vert,gor,board,k, QN);
-      printpole(board,vert,gor);
-     }
-     else cout <<"Введено неверное значение!!!"<<endl;
-    }
-    else cout <<"Невозможно поставить такое количество ферзей на доску"<<endl;
+    else cout << "РќРµРІРѕР·РјРѕР¶РЅРѕ РїРѕСЃС‚Р°РІРёС‚СЊ С‚Р°РєРѕРµ РєРѕР»РёС‡РµСЃС‚РІРѕ С„РµСЂР·РµР№ РЅР° РґРѕСЃРєСѓ" << endl;
     system("PAUSE");
     getch();
     closegraph();
 
     return 0;
 }
+
